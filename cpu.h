0223d766d8947d119e83c229c7d2652dff124f05
Heian0 <77514857+Heian0@users.noreply.github.com>
2024-04-07 12:47:45 -0700

    Added CPU clock() functionality and instruction set

--- cpu.h
+++ cpu.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <vector>
 
 class Bus;
 
@@ -53,6 +54,61 @@ class CPU {
     // ---------------- CPU Adressing Modes -----------------
 
         uint8_t IMP();  uint8_t IMM();
+        uint8_t ZP0();  uint8_t ZPX();
+        uint8_t ZPY();  uint8_t REL();
+        uint8_t ABS();  uint8_t ABX();
+        uint8_t ABY();  uint8_t IND();
+        uint8_t IZX();  uint8_t IZY();
+
+    // ------------------ CPU Opcodes ----------------------
+
+        //Legal Opcodes
+        uint8_t ADC();	uint8_t AND();	uint8_t ASL();	uint8_t BCC();
+        uint8_t BCS();	uint8_t BEQ();	uint8_t BIT();	uint8_t BMI();
+        uint8_t BNE();	uint8_t BPL();	uint8_t BRK();	uint8_t BVC();
+        uint8_t BVS();	uint8_t CLC();	uint8_t CLD();	uint8_t CLI();
+        uint8_t CLV();	uint8_t CMP();	uint8_t CPX();	uint8_t CPY();
+        uint8_t DEC();	uint8_t DEX();	uint8_t DEY();	uint8_t EOR();
+        uint8_t INC();	uint8_t INX();	uint8_t INY();	uint8_t JMP();
+        uint8_t JSR();	uint8_t LDA();	uint8_t LDX();	uint8_t LDY();
+        uint8_t LSR();	uint8_t NOP();	uint8_t ORA();	uint8_t PHA();
+        uint8_t PHP();	uint8_t PLA();	uint8_t PLP();	uint8_t ROL();
+        uint8_t ROR();	uint8_t RTI();	uint8_t RTS();	uint8_t SBC();
+        uint8_t SEC();	uint8_t SED();	uint8_t SEI();	uint8_t STA();
+        uint8_t STX();	uint8_t STY();	uint8_t TAX();	uint8_t TAY();
+        uint8_t TSX();	uint8_t TXA();	uint8_t TXS();	uint8_t TYA();
+
+        //Illegal Opcodes
+        uint8_t XXX();
+
+    // ------------------- CPU Signals ---------------------
+
+        //Clock Signal - Indicate to the CPU one clock cycle has occured.
+        void clock();
+
+        //The following 3 signals stop what the CPU is doing - although it will finish its current instruction.
+
+        //Reset
+        void reset();
+        //System Interrupt - Can be ignored if our status register has ignore on.
+        void interrupt();
+        //Non Maskable System Interrupt - Can never be ignored.
+        void non_maskable_interrupt();
+
+    // ----------------- CPU Helper Functions --------------
+
+        //Fetch Data
+        uint8_t fetch();
+        //Fetched Data
+        uint8_t fetched = 0x00;
+        //Address for directly reading from memory
+        uint16_t address_ABS = 0x0000;
+        //Relative address for branching
+        uint16_t address_REL = 0x0000;
+        //Current Opcode
+        uint8_t opcode = 0x00;
+        //Cycles remaining for this instruction.
+        uint8_t remaining_cycles = 0;
 
     // --------------- CPU -> Bus connection ---------------
 
@@ -71,6 +127,17 @@ class CPU {
 
     private:
 
+        struct instruction {
+            //Name of instruction
+            std::string name;
+            //Points to an addressing mode function
+            uint8_t(CPU::*address_mode)(void) = nullptr;
+            //Points to an operation function
+            uint8_t(CPU::*operate)(void) = nullptr;
+            //Cycles required to excecute this operation
+            uint8_t cycles;
+        };
+
         //Memory address of our bus.
         Bus* bus = nullptr;
 
@@ -78,4 +145,5 @@ class CPU {
         uint8_t getFlag(CPU_Flags flag);
         void setFlad(CPU_Flags flag, bool value);
 
+        std::vector<instruction> instructions;
 };
